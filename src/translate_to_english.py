7ea80163b00d0794a80ddb61f5901c3005dcdb60
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-02-18 07:20:49 +0000

    Translate Sheet 2

--- src/translate_to_english.py
+++ src/translate_to_english.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from transformers import MarianMTModel, MarianTokenizer
 
 # Load the MarianMT model for French to English translation
@@ -17,12 +18,13 @@ def translate_text(text):
     return translated_text
 
 
-infile = "resources/drc/f3_do_anc_tmp.xlsx"
-outfile = "resources/drc/do_anc.csv"
+infile = "../resources/drc/f3_do_anc_tmp.xlsx"
+outfile = "../resources/drc/do_anc.csv"
 xls = pd.ExcelFile(infile)
 sheets = xls.sheet_names
-# First sheet is cover page, we don't need to raed it.
-# Second sheet records information about the surveyor
+
+# First sheet is cover page, we don't need to read it.
+# Second sheet records information about the surveyor and the patient
 df = pd.read_excel(xls, sheet_name="1 Identification")
 # Read in and translate questions one-by-one
 # Note: lots of these questions have the abbrevation CPN
@@ -43,4 +45,59 @@ en_df = en_df.rename(
         "Unnamed: 1": "question_text",
     }
 )
-en_df.to_csv(outfile, index=False, encoding="utf-8")
+
+########################################################
+### Third sheet: this the set of questions used for DCO
+def format_if_number(x):
+    formatted = x
+    if isinstance(x, float):
+        formatted = "{:.2f}".format(x)
+    return formatted
+
+
+df = pd.read_excel(xls, sheet_name='2 Observation')
+questions = df["Section 2 : Observation de la consultation prénatale"]
+
+mask = questions.notna()
+# Also remove Row 1 i.e., second row as it is an instruction
+mask[1] = False
+
+questions = questions[mask]
+questions = questions.apply(format_if_number)
+
+# Remove parens and negative sign
+questions = questions.str.replace(r"[()\-]", "", regex=True)
+
+
+current_number = None
+result = []
+number_pattern = re.compile(r"^-?\d+(\.\d+)?$")
+
+# Iterate through each element in the Series
+for item in questions[0:132]:
+    # Check if the item is a number (including negatives) using the regex pattern
+    if number_pattern.match(item):
+        current_number = item
+        result.append(item)
+    else:
+        # If it's not a number, treat it as a letter
+        result.append(f"{current_number}{item.lower()}")
+
+# Convert the result back to a Pandas Series
+converted_series = pd.Series(result)
+
+questions = pd.concat([converted_series, questions[132:]])
+
+en_answers = df.loc[mask, "Unnamed: 1"].apply(translate_text)
+en_df = pd.concat([questions, en_answers], axis=1)
+en_df = en_df.rename(
+    columns={
+        "Section 2 : Observation de la consultation prénatale": "question_id",
+        "Unnamed: 1": "question_text"
+    }
+)
+
+
+
+### Finally write
+en_df.to_csv(outfile, index=False, encoding="utf-8", mode='a', header=False)
