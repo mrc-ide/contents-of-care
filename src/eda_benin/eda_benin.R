d777a5a953318eb52064ae57cb9fe8c8a103d27a
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-03-13 14:18:31 +0000

    Process Benin HF Survey

--- src/eda_benin/eda_benin.R
+++ src/eda_benin/eda_benin.R
@@ -1,108 +1,185 @@
+library(broom)
 library(dplyr)
 library(ggplot2)
+library(ggpmisc)
+library(ggpubr)
+library(glue)
 library(orderly2)
 library(purrr)
 
-theme_manuscript <- function() {
-  theme_classic() +
-    theme(
-      ## text = element_text(family = "Arial", ),
-      plot.title = element_text(size = 14, hjust = 0.5),
-      axis.title = element_text(size = 12),
-      axis.title.x = element_blank(),
-      axis.text = element_text(size = 10),
-      legend.text = element_text(size = 11),
-      legend.position = "bottom",
-      legend.title = element_blank(),
-      strip.text = element_text(size = 14),
-    )
-}
+orderly_shared_resource(utils.R = "utils.R")
+source("utils.R")
+orderly_dependency("process_benin", "latest", "benin_dco.rds")
+orderly_dependency("process_benin", "latest", "benin_facility_survey_clinical.rds")
 
-ggsave_manuscript <- function(
-    filename,
-    plot,
-    width = 6,
-    height = 4,
-    units = "in",
-    dpi = 300) {
-  ggsave(
-    filename = paste0(filename, ".svg"), plot = plot,
-    width = width, height = height, units = units
-  )
-  ggsave(
-    filename = paste0(filename, ".png"), plot = plot, width = width,
-    height = height, units = units, dpi = dpi
-  )
+benin_dco <- readRDS("benin_dco.rds")
+benin_hf <- readRDS("benin_facility_survey_clinical.rds")
 
-  ggsave(
-    filename = paste0(filename, ".pdf"), plot = plot, width = width,
-    height = height, units = units, dpi = dpi
-  )
-}
+outfile_prefix <- "benin_consult_length_by"
 
 
-orderly_dependency("process_benin", "latest","benin.rds")
-benin <- readRDS("benin.rds")
+out <- summary(benin_dco$consult_length) |> round(2) |> tidy()
 
 p1 <- gghistogram(
-  benin, x = "consult_length", add = "mean", rug = TRUE, add_density = TRUE,
-  binwidth = 5) +
+    benin_dco, x = "consult_length", add = "mean", rug = TRUE, add_density = TRUE,
+    binwidth = 5
+  ) + geom_table_npc(data = out, label = list(out), npcx = 1, npcy = 0.7) +
   labs(title = "Consultation length (minutes) in Benin") +
   theme_manuscript() +
   xlab("Consultation length (minutes)") 
 
 ggsave_manuscript("consult_length", p1)
 
-benin$first_anc <- factor(benin$first_anc)
+########### by urban/rural
+benin_dco$m0_milieu <- case_when(
+  benin_dco$m0_milieu == 1 ~ "Urban",
+  benin_dco$m0_milieu == 2 ~ "Rural",
+  TRUE ~ NA_character_
+)
 
-p2 <- gghistogram(
-  benin,
-  x = "consult_length", add = "mean", rug = TRUE, add_density = TRUE,
-  binwidth = 5, fill = "first_anc", palette = c("#00AFBB", "#E7B800"),
-  color = NA
-) +
-  labs(title = "Consultation length (minutes) in Benin") +
+p2 <- ggsummarystats(
+  data = benin_dco[! is.na(benin_dco$m0_milieu),], x = "m0_milieu", y = "consult_length",
+  ggfunc = ggboxplot, add = "jitter") 
+  
+
+p2$main.plot <- p2$main.plot +
+  stat_compare_means(label = "p.signif", comparisons = list(c("Urban", "Rural"))) +
+  ylab("Consultation length (minutes)") +
+  theme_manuscript()
+
+ggsave_manuscript(glue("{outfile_prefix}_milieu"), p2, 9, 6)
+
+########### by facility
+benin_dco$m_id1 <- factor(benin_dco$m_id1)
+## Retain facilities with at least 10 consultations
+facility_counts <- count(benin_dco, m_id1)
+morethan10 <- facility_counts$n >= 10
+benin_subset <- benin_dco[benin_dco$m_id1 %in% facility_counts$m_id1[morethan10], ]
+
+p3 <- ggsummarystats(
+  data = benin_subset, x = "m_id1", y = "consult_length",
+  ggfunc = ggboxplot, add = "jitter"
+)
+
+p3$main.plot <- p3$main.plot +
+  stat_compare_means(
+    label = "p.signif", ref.group = ".all.", label.y.npc = 0.75
+  ) +
+  ylab("Consultation length (minutes)") +
   theme_manuscript() +
-  xlab("Consultation length (minutes)")
+  xlab("Facility Id")
 
-ggsave_manuscript("consult_length_by_first_anc", p2)
+ggsave_manuscript(glue("{outfile_prefix}_facility"), p3, 9, 6)
 
-p3 <- gghistogram(
-  benin,
-  x = "consult_length", add = "mean", rug = TRUE, add_density = TRUE,
-  binwidth = 5, fill = "trimester",
-  palette = c("#000000", "#E69F00", "#56B4E9"),
-  color = NA
-) +
-  labs(title = "Consultation length (minutes) in Benin") +
+########### by health zones
+benin_dco$m_id2 <- factor(benin_dco$m_id2)
+
+p3 <- ggsummarystats(
+  data = benin_dco[!is.na(benin_dco$m_id2), ], x = "m_id2", y = "consult_length",
+  ggfunc = ggboxplot, add = "jitter"
+)
+
+p3$main.plot <- p3$main.plot +
+  stat_compare_means(
+    label = "p.signif", ref.group = ".all.", label.y.npc = 0.75
+  ) +
+  ylab("Consultation length (minutes)") +
+  theme_manuscript()
+
+ggsave_manuscript(glue("{outfile_prefix}_healthzone"), p3, 9, 6)
+
+########### by first ANC
+
+
+
+benin_dco$first_anc <- factor(benin_dco$first_anc)
+
+p4 <- ggsummarystats(
+  data = benin_dco, x = "first_anc", y = "consult_length",
+  ggfunc = ggboxplot, add = "jitter"
+)
+
+p4$main.plot <- p4$main.plot +
+  stat_compare_means(
+    label = "p.signif", label.y.npc = 0.75
+  ) +
+  ylab("Consultation length (minutes)") +
   theme_manuscript() +
-  xlab("Consultation length (minutes)")
+  xlab("First ANC visit (yes/no)")
 
-ggsave_manuscript("consult_length_by_trimester", p3)
+ggsave_manuscript(glue("{outfile_prefix}_first_anc"), p4, 9, 6)
 
+############# by trimester
+
+p5 <- ggsummarystats(
+  data = benin_dco, x = "trimester", y = "consult_length",
+  ggfunc = ggboxplot, add = "jitter"
+)
+
+p5$main.plot <- p5$main.plot +
+  stat_compare_means(
+    label = "p.signif", label.y.npc = 0.75
+  ) +
+  ylab("Consultation length (minutes)") +
+  theme_manuscript() 
+
+ggsave_manuscript(glue("{outfile_prefix}_first_anc"), p5, 9, 6)
+
+########### by time of day
+
+benin_dco$hour_start <- factor(benin_dco$hour_start)
+
+p6 <- ggsummarystats(
+  data = benin_dco, x = "hour_start", y = "consult_length",
+  ggfunc = ggboxplot, add = "jitter"
+)
+
+p6$main.plot <- p6$main.plot +
+  stat_compare_means(
+    label = "p.signif", label.y.npc = 0.75
+  ) +
+  ylab("Consultation length (minutes)") +
+  theme_manuscript() +
+  xlab("Consultation start hour")
+
+ggsave_manuscript(glue("{outfile_prefix}_hour_start"), p6, 9, 6)
+
+############
 ## Now number of steps in the consultation
 ## weight:info_complications are common for all consultations
-## If info_complications is yes, then info_placenta_previa: info_embolism are relevant
+## If info_complications is yes, then info_placenta_previa: info_embolism are rel## evant
 ## patients_age_reported:mosq_kit_prescribed for first trimester ANC
 ## uterus_measured2:fansidar_prescribed for second trimester ANC
 ## uterus_measured3:sp_ensured for third trimester ANC
 
-first_trimester <- filter(benin, trimester %in% "premier trimestre") |>
+first_trimester <- filter(benin_dco, trimester %in% "premier trimestre") |>
   select(
     consult_length, hour_start:info_embolism,
     patients_age_reported:mosq_kit_prescribed
   )
 
-second_trimester <- filter(benin, trimester %in% "deuxieme trimestre") |>
+first_trimester_steps <- select(first_trimester, weight:mosq_kit_prescribed) |>
+  colSums(na.rm = TRUE) |>
+  tidy()
+first_trimester_steps$prop <- first_trimester_steps$x / nrow(first_trimester)
+
+first_trimester_steps$names <- factor(
+  first_trimester_steps$names,
+  levels = first_trimester_steps$names, ordered = TRUE
+)
+
+
+second_trimester <- filter(benin_dco, trimester %in% "deuxieme trimestre") |>
   select(
     consult_length, hour_start:info_embolism,
     uterus_measured2:fansidar_prescribed
   )
 
 second_trimester_steps <- select(
-  second_trimester, weight:fansidar_prescribed) |>
+  second_trimester, weight:fansidar_prescribed
+) |>
   colSums(na.rm = TRUE) |>
-  broom::tidy()
+  tidy()
 
 second_trimester_steps$prop <- second_trimester_steps$x /
   nrow(second_trimester)
@@ -112,26 +189,18 @@ second_trimester_steps$names <- factor(
   levels = second_trimester_steps$names, ordered = TRUE
 )
 
-p <- ggplot(second_trimester_steps) +
-  geom_col(aes(x = names, y = prop)) +
-  xlab("") +
-  ylab("Proportion of consultations with step recorded") +
-  labs(title = "Proportion of steps taken in ANC") +
-  coord_flip() +
-  theme_manuscript()
 
-ggsave_manuscript("steps_proportion_second", p)
-
-
-third_trimester <- filter(benin, trimester %in% "troisieme trimestre") |>
+third_trimester <- filter(benin_dco, trimester %in% "troisieme trimestre") |>
   select(
     consult_length, hour_start:info_embolism, uterus_measured3:sp_ensured
   )
 
+
 third_trimester_steps <- select(
-  third_trimester, weight:sp_ensured) |>
+  third_trimester, weight:sp_ensured
+) |>
   colSums(na.rm = TRUE) |>
-  broom::tidy()
+  tidy()
 
 third_trimester_steps$prop <- third_trimester_steps$x /
   nrow(third_trimester)
@@ -140,6 +209,26 @@ third_trimester_steps$names <- factor(
   third_trimester_steps$names,
   levels = third_trimester_steps$names, ordered = TRUE
 )
+
+all_semesters <- rbind(
+  mutate(first_trimester_steps, trimester = "First"),
+  mutate(second_trimester_steps, trimester = "Second"),
+  mutate(third_trimester_steps, trimester = "Third")
+)
+
+p <- ggplot(all_semesters) +
+  geom_col(aes(x = names, y = prop)) +
+  xlab("") +
+  scale_x_discrete(limits = rev) +
+  facet_wrap(~trimester, ncol = 3) +
+  ylab("Proportion of consultations with step recorded") +
+  labs(title = "Proportion of steps taken in ANC") +
+  coord_flip() +
+  theme_manuscript()
+
+ggsave_manuscript("steps_proportion_second", p)
+
+
 
 p <- ggplot(third_trimester_steps) +
   geom_col(aes(x = names, y = prop)) +
@@ -157,8 +246,6 @@ ggsave_manuscript("steps_proportion_third", p)
 
 
 ## Now we count the number of steps recorded as having been taken
-first_trimester <- rowwise(first_trimester) |>
-  mutate(nsteps = sum(c_across(weight:info_complications), na.rm = TRUE))
 
 
 nsteps <- map_dfr(
@@ -166,7 +253,7 @@ nsteps <- map_dfr(
   function(x) {
   out <- select(x, weight:info_complications) |>
     colSums(na.rm = TRUE) |>
-    broom::tidy()
+    tidy()
   out$prop <- out$x / nrow(x)
   out
   }, .id = "trimester")
@@ -178,8 +265,10 @@ nsteps$names <- factor(
     "abdominal_palpation", "lower_limb_edema", "vaginal_exam", "speculum_exam",
     "blood_pressure", "albuminuria", "glucoserie", "us_requested",
     "info_complications", "info_placenta_previa", "info_premature_rupture",
-    "info_postpartum_hemorrhage", "info_retained_placenta", "info_uterus_inverted",
-    "info_ectopic_pregnancy", "info_hemorrhage_antepartum", "info_prolonged_labor_latent",
+    "info_postpartum_hemorrhage", "info_retained_placenta",
+    "info_uterus_inverted",
+    "info_ectopic_pregnancy", "info_hemorrhage_antepartum",
+    "info_prolonged_labor_latent",
     "info_prolonged_labor_second", "info_cpd", "info_uterine_rupture",
     "info_abnormal_presentation", "info_infection", "info_uterine_perforation",
     "info_hypertensive_crises", "info_ecclampsia", "info_pre_eclampsia",
--- src/process_benin/process_benin.R
+++ src/process_benin/process_benin.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(orderly2)
 ## Data dictionary available at
 ## https://microdata.worldbank.org/index.php/catalog/2176/data-dictionary
+
 ## Lemière, C., & de Walque, D. (2014). Health Results-Based Financing Impact
 ## Evaluation Survey 2010-2011, Baseline [Data set]. World Bank, Development Data
 ## Group. https://doi.org/10.48529/QKMG-R734
@@ -84,17 +85,26 @@ benin <- rename(
   ## m3_6 is about ensuring SP.
   ## In the questionnaire, m3_5 is stage of pregnancy and m3_6 is
   ## about ensuring SP.
-  ## In the data file m3_4 encodes pregnancy stage
+  ## In the data file m3_4 encodes pregnancy stage. I think m3_4 and m3_5 are
+  ## swapped in the data; that is:
+  ## m3_5 is sp_prescribed
+  ## m3_4 is pregnancy_stage
+  ## m3_6 is
+  ## "Did the provider make sure that the patient had already (or had not yet)
+  ## taken SP?"
   uterus_measured3 = m3_1,
   fetal_heartbeat_checked3 = m3_2,
-  fansidar_given3 = m3_3,
+  sp_given3 = m3_3,
   pregnancy_stage = m3_4,
-  sp_ensured = m3_6
+  ## If m3_3 and m3_ are both no, then m3_6 is not applicable
+  sp_prescribed3 = m3_5,
+  sp_ensured3 = m3_6
 )
 
 
+
 ## Answers to pregnancy_stage are at most 36 weeks, and more than 36 weeks
-benin <- relocate(benin, pregnancy_stage, .after = sp_ensured)
+benin <- relocate(benin, pregnancy_stage, .after = sp_ensured3)
 
 start <- hm(paste(benin$hour_start, benin$min_start, sep = ":"))
 end <- hm(paste(benin$hour_end, benin$min_end, sep = ":"))
@@ -102,7 +112,127 @@ end <- hm(paste(benin$hour_end, benin$min_end, sep = ":"))
 benin$consult_length <- time_length(end - start, unit = "minute")
 
 
-benin <- mutate(benin, across(first_anc:sp_ensured, recode_oui_non))
-##benin <- rowwise(benin) |> mutate(nsteps = sum(c_across(m0_03:m3_6), na.rm = TRUE))
+benin <- mutate(benin, across(first_anc:sp_ensured3, recode_oui_non))
+## benin <- rowwise(benin) |> mutate(nsteps = sum(c_across(m0_03:m3_6), na.rm = TRUE))
 
-saveRDS(benin, "benin.rds")
+saveRDS(benin, "benin_dco.rds")
+orderly_artefact(
+  files = "benin_dco.rds",
+  description = "Benin direct clinical observations survey"
+)
+
+## Process Health facility survey (part 2: clinical aspects)
+## https://microdata.worldbank.org/index.php/catalog/2176/data-dictionary/F29?file_name=benhrbf2_f
+infile <- "benhrbf2_f.dta"
+benin_hfds <- orderly_shared_resource(benin_hfds.dta = paste0(indir, infile))
+facility_survey_cl <- read.dta("benin_hfds.dta")
+facility_survey_cl <- rename(
+  facility_survey_cl,
+  catchment_population = f1_1,
+  catchment_population_female_15_49 = f1_2,
+  catchment_population_under_1 = f1_3,
+  catchment_population_under_5 = f1_4,
+  total_attendance_2009 = f1_5,
+  new_patients_2009 = f1_6,
+  new_female_patients_2009 = f1_7,
+  new_pregrant_patients_2009 = f1_8,
+  new_patients_under_5_2009 = f1_9,
+  vaccinations_provided = f2_1,
+  vaccinations_room = f2_2,
+  vaccinations_children_regulary = f2_3,
+  vaccinations_workplan_2009 = f2_4,
+  ## Go to reproductive health section now
+  anc_offered = f3_1,
+  anc_visits_2009 = f3_2,
+  number_first_anc_before_first_trimester = f3_3,
+  number_at_least_4_anc = f3_4,
+  ## Procedures offered at every visit
+  info_complications = f3_5a,
+  patients_weight = f3_5b,
+  blood_pressure = f3_5c,
+  blood_test = f3_5d,
+  urine_test = f3_5e,
+  patients_height = f3_5f,
+  abdominal_palpation = f3_5g,
+  nutritional_advice = f3_5h,
+  sp_administration = f3_5j,
+  ultrasound = f3_5k,
+  ## Procedures performed during the first trimester or first prenatal visit
+  patients_age = f3_6a,
+  patients_height2 = f3_6b,
+  patients_weight2 = f3_6c,
+  inspection2 = f3_6d,
+  abdominal_palpation2 = f3_6e,
+  vaginal_exam2 = f3_6f,
+  speculum_exam2 = f3_6g,
+  blood_pressure2 = f3_6h,
+  blood_test2 = f3_6i,
+  urine_test2 = f3_6j,
+  vaccination_advice2 = f3_6k,
+  nutritional_advice2 = f3_6l,
+  number_previous_pregnancies2 = f3_6m,
+  ultrasound2 = f3_6n,
+  ## MII most likely means mosquito net
+  mii = f3_6o,
+  other2 = f3_6p,
+  ## Procedures performed during the second trimester
+  patients_weight3 = f3_7a,
+  inspection3 = f3_7b,
+  abdominal_palpation3 = f3_7c,
+  vaginal_exam3 = f3_7d,
+  speculum_exam3 = f3_7e,
+  blood_pressure3 = f3_7f,
+  blood_test3 = f3_7g,
+  urine_test3 = f3_7h,
+  uterus_measured3 = f3_7i,
+  ultrasound3 = f3_7j,
+  vaccination_verification3 = f3_7k,
+  ## Procedures performed during the third trimester
+  patients_weight4 = f3_8a,
+  inspection4 = f3_8b,
+  abdominal_palpation4 = f3_8c,
+  vaginal_exam4 = f3_8d,
+  speculum_exam4 = f3_8e,
+  blood_pressure4 = f3_8f,
+  blood_test4 = f3_8g,
+  urine_test4 = f3_8h,
+  uterus_measured4 = f3_8i,
+  ultrasound4 = f3_8j,
+  pelvic_exam4 = f3_8k,
+  ## skipping over some less relevant questions
+  number_women_vaccinated_tetanus = f3_12, ## last 6 months
+  number_women_vaccinated_tetanus_2 = f3_13, ## 2009
+  number_women_sp_second_dose = f3_14, ## in the required timeframe
+  number_women_itn = f3_15, ## in 2009
+  hiv_test_offered = f3_16, 
+  pregnant_women_private_space = f3_17, ##
+  ## Are the following equipment and materials available and functional
+  consultation_table = f3_18a,
+  blood_pressure_monitor = f3_18b,
+  stethoscope = f3_18c,
+  measuring_tape = f3_18d,
+  weighing_scale_with_height = f3_18e,
+  fetoscope = f3_18f,
+  gloves_not_torn = f3_18g,
+  births_assisted_24_7 = f3_19,
+  number_of_days_stay_afetr_birth = f3_20,
+  number_of_days_premature_no_complications = f3_21,
+  number_of_women_referred_by_hf = f3_22,
+  number_of_women_referred_to_hf = f3_25,
+  avg_length_of_stay_if_complication = f3_29,
+  number_of_maternal_deaths = f3_30, ## last 12 months
+  number_of_births_2009 = f3_32,
+  csec_offered = f3_39,
+  csec_trained_personnel = f3_40,
+  number_csec_trained_personnel = f3_40a,
+  number_of_beds_for_delivery = f3_48,
+  women_in_labour_seen_by_patients = f3_49,
+  women_in_labour_heard_by_patients = f3_50,
+  women_in_labour_pay = f3_51
+)
+
+saveRDS(facility_survey_cl, "benin_facility_survey_clinical.rds")
+orderly_artefact(
+  files = "benin_facility_survey_clinical.rds",
+  description = "Benin facility survey clinical survey"
+)
