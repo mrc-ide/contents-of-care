95a658a8238ab05ed52353b53c3cb4df0798b893
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-05-14 14:07:02 +0100

    Standard EDA

---
+++ src/eda_drc_midline/eda_drc_midline.R
@@ -0,0 +1,74 @@
+library(broom)
+library(dplyr)
+library(ggplot2)
+library(ggpmisc)
+library(ggpubr)
+library(glue)
+library(knitr)
+library(orderly2)
+library(purrr)
+library(scales)
+library(snakecase)
+library(table1)
+
+orderly_shared_resource("utils.R")
+source("utils.R")
+## Notes:
+## 33 rows with consult_length > 75 minutes i.e. 97.5th percentile
+## I have checked the times, and there are no obvious data entry
+## errors; so likely these are real outliers.
+orderly_dependency(
+  "process_drc_midline", "latest", files = c("drc_midline_dco.rds")
+)
+
+drc_midline_dco <- readRDS("drc_midline_dco.rds")
+outfile_prefix <- "drc_midline_consult_length_by"
+
+out <- summary(drc_midline_dco$consult_length_calc) |>
+  round(2) |>
+  tidy()
+
+p1 <- gghistogram(
+  drc_midline_dco,
+  x = "consult_length_calc", add = "mean", rug = TRUE, add_density = TRUE,
+  binwidth = 5
+) + geom_table_npc(data = out, label = list(out), npcx = 1, npcy = 0.7) +
+  labs(title = "Consultation length (minutes) in DRC") +
+  theme_pubr() +
+  xlab("Consultation length (minutes)")
+
+outfile <- glue("{outfile_prefix}_overall")
+ggsave_manuscript(outfile, p1)
+orderly_artefact(
+  description = "Consultation length in DRC", files = glue("{outfile}.png")
+)
+
+cat_covariates <- c(
+  "province", 
+  "num_prev_anc_visits", "trimester",
+  "first_pregnancy", "first_anc", 
+  "hcw_sex", "hcw_qualification", "consultation_language"
+)
+
+
+walk(cat_covariates, function(x) {
+  f <- as.formula((paste("~ consult_length_calc |", x)))
+  small_df <- drc_midline_dco[!is.na(drc_midline_dco[[x]]), ]
+  small_df[[x]] <- factor(small_df[[x]])
+  label(small_df$consult_length_calc) <- to_title_case(x)
+  tab <- table1(
+    f, data = small_df, extra.col = list("p-value" = render_p_value_cat),
+    overall = FALSE, transpose = FALSE, render.cont = my.render.cont,
+    caption = to_title_case(x),
+  )
+  kable(as.data.frame(tab), format = "latex", booktabs = TRUE) |>
+    writeLines(glue("drc_midline_dco_{x}.tex"))
+ })
+
+outfiles <- glue("drc_midline_dco_{cat_covariates}.tex")
+
+orderly_artefact(
+  description = "Consultation length by categorical covariates",
+  files = outfiles
+)
+
