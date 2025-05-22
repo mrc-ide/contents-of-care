f388d06dfa348a4a4c389af4f85f628abc52228f
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-05-22 16:01:45 +0100

    DRC Midline Survey highlights

--- src/coc-summary/coc-summary.R
+++ src/coc-summary/coc-summary.R
@@ -36,5 +36,14 @@ orderly_dependency(
   "lm_drc", "latest",c("drc_2015_dco_lasso_coefficients.png")
 )
 
+
+orderly_dependency(
+  "eda_drc_midline", "latest", c("drc_midline_consult_length_by_overall.png")
+)
+
+orderly_dependency(
+  "lm_drc_midline", "latest", c("drc_midline_dco_lasso_coefficients.png")
+)
+
 ##quarto_render("summary-presentation.qmd")
 system("pdflatex main.tex")
