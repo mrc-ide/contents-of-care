2156eb662caa1fd4f0dd04511fbcbe35ca24bfe4
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-08-26 17:10:11 +0100

    DRC 2021 Model Fitting

---
+++ src/lm_drc_endline/lm_drc_endline.R
@@ -0,0 +1,31 @@
+library(broom)
+library(dplyr)
+library(ggforce)
+library(ggplot2)
+library(glue)
+library(ggpmisc)
+library(glmnet)
+library(glue)
+library(janitor)
+library(lubridate)
+library(orderly2)
+library(purrr)
+library(rsample)
+library(scales)
+library(tibble)
+library(tidylog)
+library(tidyr)
+
+orderly_shared_resource("utils.R")
+source("utils.R")
+
+
+
+orderly_dependency("process_drc_endline", "latest", "drc_endline_split.rds")
+drc_endline_split <- readRDS("drc_endline_split.rds")
+
+set.seed(42)
+orderly_resource("lm_drc_multilevel.R")
+source("lm_drc_multilevel.R")
+
+
---
+++ src/lm_drc_endline/lm_drc_multilevel.R
@@ -0,0 +1,24 @@
+
+library(brms)
+
+fits <- map(drc_baseline_split, function(x) {
+  brm(
+    formula = bf(log_consult_length ~ . -province + (1 | province)),
+    data = x,
+    family = gaussian(),
+    drop_unused_levels = TRUE,
+    chains = 4,
+    cores = 4,
+    iter = 4000,
+    prior = prior_spec,
+    control = list(adapt_delta = 0.99)
+)})
+
+saveRDS(fits, file = "drc_endline_fits.rds")
+orderly_artefact(
+  files = "drc_endline_fits.rds",
+  description = "brms models for DRC endline data"
+)
+
+
+
