e6c02a1ec050f8fc9e58419d3c542461531f07cb
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-10-23 12:58:25 +0100

    Scale before model fitting

--- src/lm_burkina_faso_endline/lm_burkina_faso_endline.R
+++ src/lm_burkina_faso_endline/lm_burkina_faso_endline.R
@@ -26,6 +26,28 @@ orderly_dependency(
 )
 
 bfa_split <- readRDS("bfa_endline_split.rds")
+cols_to_scale <- grep("scaled", names(bfa_split[[1]]), value = TRUE)
+x <- bind_rows(bfa_split)
+
+centers <- sapply(cols_to_scale, \(col) mean(x[[col]], na.rm = TRUE))
+scales <- sapply(cols_to_scale, \(col) sd(x[[col]], na.rm = TRUE))
+
+x <- mutate(x, across(all_of(cols_to_scale), \(v) as.numeric(scale(v))))
+
+scaled_attrs <- data.frame(
+  variable = names(centers),
+  center   = unname(centers),
+  scale    = unname(scales)
+)
+
+saveRDS(scaled_attrs, file = "scaled_attributes.rds")
+orderly_artefact(
+  files = "scaled_attributes.rds",
+  description = "scaled_attributes"
+)
+
+bfa_split <- split(x, list(x$first_anc, x$trimester), sep = "_")
+
 
 orderly_resource("lm_burkina_faso_endline_multilevel.R")
 source("lm_burkina_faso_endline_multilevel.R")
