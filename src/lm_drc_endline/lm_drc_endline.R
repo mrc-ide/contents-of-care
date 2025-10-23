518723e093505bd43e6d988b7fc8ca4889ce5a25
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-10-23 12:47:06 +0100

    Scale before model fitting

--- src/lm_drc_endline/lm_drc_endline.R
+++ src/lm_drc_endline/lm_drc_endline.R
@@ -24,6 +24,29 @@ if (pars[["debug"]]) iter <- 10 else iter <- 4000
 orderly_dependency("process_drc_endline", "latest", "drc_endline_split.rds")
 drc_endline_split <- readRDS("drc_endline_split.rds")
 
+cols_to_scale <- grep("scaled", names(drc_endline_split[[1]]), value = TRUE)
+x <- bind_rows(drc_endline_split)
+
+centers <- sapply(cols_to_scale, \(col) mean(x[[col]], na.rm = TRUE))
+scales <- sapply(cols_to_scale, \(col) sd(x[[col]], na.rm = TRUE))
+
+x <- mutate(x, across(all_of(cols_to_scale), \(v) as.numeric(scale(v))))
+
+scaled_attrs <- data.frame(
+  variable = names(centers),
+  center   = unname(centers),
+  scale    = unname(scales)
+)
+
+saveRDS(scaled_attrs, file = "scaled_attributes.rds")
+orderly_artefact(
+  files = "scaled_attributes.rds",
+  description = "scaled_attributes"
+)
+
+drc_endline_split <- split(x, list(x$first_anc, x$trimester), sep = "_")
+
+
 factor_vars <- c(
   "province",
   "facility_level_mapping",
