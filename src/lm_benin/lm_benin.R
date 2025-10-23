9129c281c4ba3ab5ab97a69855d5d1972579d8f2
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-10-23 12:16:37 +0100

    Scale variables right before model fitting

--- src/lm_benin/lm_benin.R
+++ src/lm_benin/lm_benin.R
@@ -2,15 +2,10 @@ library(broom)
 library(brms)
 library(cli)
 library(dplyr)
-library(ggplot2)
-library(ggpmisc)
-library(glmnet)
 library(glue)
 library(orderly2)
 library(purrr)
-library(rsample)
 library(scales)
-library(snakecase)
 library(tibble)
 library(tidylog)
 library(tidyr)
@@ -21,11 +16,34 @@ library(tidyr)
 orderly_shared_resource(utils.R = "utils.R")
 source("utils.R")
 
-pars <- orderly_parameters(debug = TRUE, sample_prior = "no")
+##pars <- orderly_parameters(debug = TRUE, sample_prior = "no")
 if (pars[["debug"]]) iter <- 10 else iter <- 8000
 
 orderly_dependency("process_benin", "latest", files = c("benin_split.rds"))
 benin_split <- readRDS("benin_split.rds")
+
+cols_to_scale <- grep("scaled", names(benin_split[[1]]), value = TRUE)
+x <- bind_rows(benin_split)
+
+centers <- sapply(cols_to_scale, \(col) mean(x[[col]], na.rm = TRUE))
+scales <- sapply(cols_to_scale, \(col) sd(x[[col]], na.rm = TRUE))
+
+x <- mutate(x, across(all_of(cols_to_scale), \(v) as.numeric(scale(v))))
+
+scaled_attrs <- data.frame(
+  variable = names(centers),
+  center   = unname(centers),
+  scale    = unname(scales)
+)
+
+saveRDS(scaled_attrs, file = "scaled_attributes.rds")
+orderly_artefact(
+  files = "scaled_attributes.rds",
+  description = "scaled_attributes"
+)
+
+benin_split <- split(x, list(x$first_anc, x$trimester))
+
 
 factor_vars <- c(
   "milieu_of_residence", "health_zone", "facility_level_mapping",
