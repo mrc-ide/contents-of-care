d7bd65f05e7d218dbdc5e90b6e0f3dc9482a8af1
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2025-10-23 12:59:25 +0100

    Scale before fitting

--- src/lm_burkina_faso_baseline/lm_burkina_faso_baseline.R
+++ src/lm_burkina_faso_baseline/lm_burkina_faso_baseline.R
@@ -21,11 +21,30 @@ if (pars[["debug"]]) iter <- 10 else iter <- 8000
 
 
 orderly_dependency(
-  "process_burkina_faso", "latest",
-  files = c("bfa_baseline_split.rds")
+  "process_burkina_faso", "latest", files = c("bfa_baseline_split.rds")
 )
 
 bfa_split <- readRDS("bfa_baseline_split.rds")
+cols_to_scale <- grep("scaled", names(bfa_split[[1]]), value = TRUE)
+x <- bind_rows(bfa_split)
+
+centers <- sapply(cols_to_scale, \(col) mean(x[[col]], na.rm = TRUE))
+scales <- sapply(cols_to_scale, \(col) sd(x[[col]], na.rm = TRUE))
+
+x <- mutate(x, across(all_of(cols_to_scale), \(v) as.numeric(scale(v))))
+
+scaled_attrs <- data.frame(
+  variable = names(centers), center   = unname(centers), scale = unname(scales)
+)
+
+saveRDS(scaled_attrs, file = "scaled_attributes.rds")
+orderly_artefact(
+  files = "scaled_attributes.rds",
+  description = "scaled_attributes"
+)
+
+bfa_split <- split(x, list(x$first_anc, x$trimester), sep = "_")
+
 
 orderly_resource("lm_burkina_faso_baseline_multilevel.R")
 source("lm_burkina_faso_baseline_multilevel.R")
